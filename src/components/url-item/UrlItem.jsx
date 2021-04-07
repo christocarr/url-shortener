eb594eb8b2e7b1d0e4ad2f6db872846a559a451a
christocarr <christocarr@outlook.com>
2021-04-07 18:40:49 +0100

    style: add various app wide styling

--- src/components/url-item/UrlItem.jsx
+++ src/components/url-item/UrlItem.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
+import styles from './UrlItem.module.css';
 
 function UrlItem({ index, linkAndUrl, copiedShortenedLink, handleCopy }) {
   const { url, shortenedLink } = linkAndUrl;
+  console.log(index);
   return (
-    <li key={index}>
-      <div>{url}</div>
-      <div>
-        <p>{shortenedLink}</p>
+    <li key={index} className={styles.list_item}>
+      <div className={styles.user_url_container}>
+        <p>{url}</p>
+      </div>
+      <div className={styles.shortened__url_link_container}>
+        <p className={styles.short_link}>{shortenedLink}</p>
         {shortenedLink === copiedShortenedLink ? (
-          <button style={{ backgroundColor: 'hsl(257, 27%, 26%)' }}>
+          <button
+            className={styles.copy_button}
+            style={{ backgroundColor: 'hsl(257, 27%, 26%)' }}
+          >
             Copied!
           </button>
         ) : (
-          <button onClick={handleCopy}>Copy</button>
+          <button onClick={handleCopy} className={styles.copy_button}>
+            Copy
+          </button>
         )}
       </div>
     </li>
