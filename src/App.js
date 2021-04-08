8f186d9b5144ff1797095be9d85995edc38c3361
christocarr <christocarr@outlook.com>
2021-04-08 09:37:10 +0100

    feat: add statistics section

--- src/App.js
+++ src/App.js
@@ -49,6 +49,63 @@ function App() {
           </ul>
         )}
       </div>
+      <section className="statistics_section">
+        <h2>Advanced Statistics</h2>
+        <p>
+          Track how your links are performing across the web with our advanced
+          statistics dashboard.
+        </p>
+        <div className="statistics_container">
+          <div
+            className="icon_container"
+            style={{
+              backgroundImage: `url('./assets/images/icon-brand-recognition.svg')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              backgroundSize: '50%',
+            }}
+          ></div>
+          <h3>Brand Recognition</h3>
+          <p>
+            Boost your brand recognition with each click. Generic links don't
+            mean a thing. Branded links help instil confidence in your content.
+          </p>
+        </div>
+
+        <div className="statistics_container">
+          <div
+            className="icon_container"
+            style={{
+              backgroundImage: `url('./assets/images/icon-detailed-records.svg')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              backgroundSize: '50%',
+            }}
+          ></div>
+          <h3>Detaild Records</h3>
+          <p>
+            Gain insights into who is clicking your links. Knowing when and
+            where people engage with your content helps inform better decisions.
+          </p>
+        </div>
+        <div className="connection_line"></div>
+        <div className="statistics_container">
+          <div
+            className="icon_container"
+            style={{
+              backgroundImage: `url('./assets/images/icon-fully-customizable.svg')`,
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center',
+              backgroundSize: '50%',
+            }}
+          ></div>
+          <h3>Fully Customizable</h3>
+          <p>
+            Improve brand awareness and content discoverability through
+            customizable links, supercharging audience engagement.{' '}
+          </p>
+        </div>
+      </section>
     </>
   );
 }
