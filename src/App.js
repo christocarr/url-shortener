411035407eba046dc981817bc6f97b5c6a014e33
christocarr <christocarr@outlook.com>
2021-04-08 08:16:50 +0100

    feat: add hero section

--- src/App.js
+++ src/App.js
@@ -11,24 +11,45 @@ function App() {
   };
 
   return (
-    <div>
-      <UrlShortenerForm
-        shortLinkAndUrl={shortLinkAndUrl}
-        setShortLinkAndUrl={setShortLinkAndUrl}
-      />
-      {shortLinkAndUrl.length > 0 && (
-        <ul>
-          {shortLinkAndUrl.map((linkAndUrl, index) => (
-            <UrlItem
-              index={index}
-              linkAndUrl={linkAndUrl}
-              copiedShortenedLink={copiedShortenedLink}
-              handleCopy={handleCopy}
-            />
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <section className="hero_container">
+        <div
+          className="svg_container"
+          style={{
+            backgroundImage: `url('./assets/images/illustration-working.svg')`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'calc(100% + 110px)',
+            backgroundSize: '100%',
+          }}
+        ></div>
+        <div className="hero_text_container">
+          <h1 className="hero_heading">More than just shorter links</h1>
+          <p className="hero_body">
+            Build your brand's recognition and get insights on how your links
+            are performing.
+          </p>
+          <button className="hero_cta">Get Started</button>
+        </div>
+      </section>
+      <div className="form_link_list_container">
+        <UrlShortenerForm
+          shortLinkAndUrl={shortLinkAndUrl}
+          setShortLinkAndUrl={setShortLinkAndUrl}
+        />
+        {shortLinkAndUrl.length > 0 && (
+          <ul>
+            {shortLinkAndUrl.map((linkAndUrl, index) => (
+              <UrlItem
+                index={index}
+                linkAndUrl={linkAndUrl}
+                copiedShortenedLink={copiedShortenedLink}
+                handleCopy={handleCopy}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
 
