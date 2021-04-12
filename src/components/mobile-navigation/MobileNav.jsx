381bc0124a70c64c9f12934799bbd4826ec0fea6
christocarr <christocarr@outlook.com>
2021-04-12 14:19:18 +0100

    feat: navigation menu for mobile screens

--- src/components/mobile-navigation/MobileNav.jsx
+++ src/components/mobile-navigation/MobileNav.jsx
@@ -6,21 +6,25 @@ function MobileNav() {
     <>
       <nav className={styles.mobile_nav}>
         <ul role="menu" className={styles.mobile__nav_list}>
-          <li>
-            <a href="#">Features</a>
-          </li>
-          <li>
-            <a href="#">Pricing</a>
-          </li>
-          <li>
-            <a>Resources</a>
-          </li>
-          <li>
-            <a href="#">Login</a>
-          </li>
-          <li>
-            <SignupButton buttonWidth="100%">Sign Up</SignupButton>
-          </li>
+          <div className={styles.nav__items_container}>
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+          </div>
+          <div className={styles.account__items_container}>
+            <li>
+              <a href="#">Login</a>
+            </li>
+            <li>
+              <SignupButton buttonWidth="100%">Sign Up</SignupButton>
+            </li>
+          </div>
         </ul>
       </nav>
     </>
